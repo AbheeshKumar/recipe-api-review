a97d4d7cc93059db151ed3abedbacefb54fc2344
AbheeshKumar <abheshkhemani@gmail.com>
2024-12-12 17:58:21 +0500

    Django Template

--- app/core/models.py
+++ app/core/models.py
@@ -1,17 +1,31 @@
 from django.db import models
-from django.contrib.auth.models import BaseUserManager,  AbstractBaseUser, PermissionsMixin
-
-# Create your models here.
+from django.contrib.auth.models import (
+    AbstractBaseUser,
+    BaseUserManager,
+    PermissionsMixin,
+)
 
 class UserManager(BaseUserManager):
     def create_user(self, email, password=None, **extra_fields):
-        """Create, Save and Return a new user"""
+        if not email:
+            raise ValueError("User must have an email address")
+
         user = self.model(email=self.normalize_email(email), **extra_fields)
         user.set_password(password)
         user.save(using=self._db)
 
         return user
 
+    def create_superuser(self,email,password):
+        user = self.create_user(email,password)
+        user.is_staff = True
+        user.is_superuser = True
+        user.save(using=self._db)
+
+        return user
+
+#AbstractBaseUser adds auth
+#PermissionMixing adds permissions
 class User(AbstractBaseUser, PermissionsMixin):
     email = models.EmailField(max_length=255, unique=True)
     name = models.CharField(max_length=255)
@@ -20,4 +34,4 @@ class User(AbstractBaseUser, PermissionsMixin):
 
     objects = UserManager()
 
-    USERNAME_FIELD = 'email'
+    USERNAME_FIELD = "email"
--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -1,29 +1,44 @@
-from unittest import TestCase
+from django.test import TestCase
 from rest_framework.authentication import get_user_model
 
-
-class TestModel(TestCase):
-
-    def create_user(self, email, password):
-        return get_user_model().objects.create_user(
-            email=email,
-            password=password
+def create_user(email="abhesh@example.com", password="password123"):
+    return get_user_model().objects.create_user(
+        email=email,
+        password=password
     )
 
+class ModelTests(TestCase):
     def test_user_creation(self):
         email = "abheesh@example.com"
         password = "Password123"
 
         user = get_user_model().objects.create_user(email=email, password=password)
-
         self.assertEqual(email, user.email)
         self.assertTrue(user.check_password(password))
 
     def test_normalized_emails(self):
-        emails = ["abheeshk@example.com", "abheeshK@EXAMPLE.com", "ABHEESHK@example.com", "abheeshk@exmaple.COM","abheeshk@ExAmPle.COM"]
+        emails = ["abheeshk@example.com", "abheeshk@EXAMPLE.com", "abheeshk@example.COM","abheeshk@ExAmPle.COM"]
         password = "Password123"
+        correct_email = "abheeshk@example.com"
 
         for email in emails:
-            user = self.create_user(email, password)
-            self.assertEqual(user.email, email)
-            self.assertEqual(user.check_password(password))
+            user = create_user(email, password)
+            self.assertEqual(user.email, correct_email)
+            self.assertTrue(user.check_password(password))
+            user.delete()
+
+    def test_empty_email(self):
+        with self.assertRaises(ValueError):
+            get_user_model().objects.create_user(
+                email='',
+                password='password123'
+            )
+
+    def test_superuser_creation(self):
+        user = get_user_model().objects.create_superuser(
+            email="abhesh@example.com",
+            password="password123"
+        )
+
+        self.assertTrue(user.is_superuser)
+        self.assertTrue(user.is_staff)
